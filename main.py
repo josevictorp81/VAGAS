a11448a171f9bf5b64e9534acc35f4be4c8044d0
Jose Victor Pereira Lima <josevictorpy@gmail.com>
2022-09-28 17:00:19 -0300

    chamando método de buscar vagas

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
+from src.search.search import BaseSearch
 
 class Main:
     def run():
-        print('test')
-
+        # BaseSearch.search_jobs(stack='django')
+        # BaseSearch.search_jobs(stack='node')
+        # BaseSearch.search_jobs(stack='ruby')
+        BaseSearch.search_jobs(stack='back no label')
+        print(True)
 
 if __name__ == '__main__':
     Main.run()
