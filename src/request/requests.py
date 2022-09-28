a92372052001a3242eb5310265bcd1dfd3bdc3dd
Jose Victor Pereira Lima <josevictorpy@gmail.com>
2022-09-28 10:15:47 -0300

    classe de requisição http request

---
+++ src/request/requests.py
@@ -0,0 +1,16 @@
+import requests
+
+HEADERS = {
+    'user-agent': 'Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Mobile Safari/537.36',
+    'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+    'accept-language': 'pt-BR,pt;q=0.9,en-US;q=0.8,en;q=0.7',
+    'connection': 'close'
+}
+
+class Requests:
+    def __init__(self, url: str) -> None:
+        self.__requests = requests
+        self.__url = url
+
+    def get(self):
+        return self.__requests.get(url=self.__url, headers=HEADERS)
